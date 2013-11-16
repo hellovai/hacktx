c0bc3642b985243093c38b0467c5d276b60e6f12
Vaibhav Gupta <kacxdak@gmail.com>
2013-11-16 13:39:46 -0600

    adding correct direction

--- backend/public/js/socket_front.js
+++ backend/public/js/socket_front.js
@@ -3,7 +3,7 @@ function writeMessage (string, cls) {
 	$("#chat-convo").scrollTop($("#chat-convo")[0].scrollHeight);
 }
 
-var socket = io.connect('http://mealmaniac:8080');
+var socket = io.connect('http://www.mealmaniac.com:8080');
 
 // on load of page
 $(function(){
