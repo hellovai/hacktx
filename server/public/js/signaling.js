15ca4e86426cb4f5fc13f8fa8a4d098641e579e8
Vaibhav Gupta <kacxdak@gmail.com>
2013-12-27 09:05:12 -0600

    clean up

--- server/public/js/signaling.js
+++ server/public/js/signaling.js
@@ -1,4 +1,4 @@
-var webrtc = new SimpleWebRTC({
+var socket = new SimpleWebRTC({
     // the id/element dom element that will hold "our" video
     localVideoEl: 'self-video',
     // the id/element dom element that will hold remote videos
@@ -8,10 +8,9 @@ var webrtc = new SimpleWebRTC({
     //url for call
     url: "http://mealmaniac.com:3000"
 });
-var socket = webrtc;
 
 var RTCready = false;
-webrtc.on('readyToCall', function () {
+socket.on('readyToCall', function () {
 	RTCready = true;
 });
 
@@ -21,10 +20,10 @@ function joinWebRTC (roomname, delay) {
 	else if(!RTCready)
 		setTimeout(joinWebRTC(roomname, delay * 2), delay );
 	else
-		webrtc.joinRoom(roomname);
+		socket.joinRoom(roomname);
 }
 
 function leaveWebRTC () {
 	if(RTCready)
-		webrtc.leaveRoom()
+		socket.leaveRoom()
 }
